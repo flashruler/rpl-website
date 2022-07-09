7c712959ae9a4b07fa6a9cfbc32ceeaf7701f6eb
Jay Buensuceso <jbuensuceso@ucsd.edu>
2022-07-09 13:53:28 -0700

    Update phoenix.js

--- pages/phoenix.js
+++ pages/phoenix.js
@@ -66,10 +66,7 @@ export default function phoenix() {
         </div>
         <div className="flex flex-col items-center w-3/4 my-72">
           <h1 className="text-white text-3xl font-light">Structures</h1>
-          <h1 className="text-white text-xl font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-           ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-           dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+          <h1 className="text-white text-xl font-thin">Hel. We have no idea what we are doing. We feel like we are trapped in Zachs D&D RPL campaign. Save us.</h1>
 
           <Link
               href="/phoenix"
